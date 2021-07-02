e99c2a7de2090b6a785b0e090a24863dac119c14
onionyzl <17670843262@163.com>
2021-07-02 15:48:51 +0800

    updated linear classification

--- main_cls.py
+++ main_cls.py
@@ -5,15 +5,18 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torchvision.datasets
+import logging
+import tensorboardX
+import shutil
 
 from torch.utils.data import DataLoader
 from model.encoder import get_model
 from utils import AverageMeter, accuracy
 
-
+best_acc1 = 0.0
 DIR = {
     'DATA': '..\..\dataset',
-    'CHECKPOINT': '.\checkpoint',
+    'CLS_CHECKPOINT': '.\cls_checkpoint',
     'LOG': '.\log',
     'WRITER': '.\writer'
 }
@@ -42,8 +45,12 @@ parser.add_argument('--dim', metavar='N', type=int, default=10, help='number of 
 
 
 def main():
+    global best_acc1
     args = parser.parse_args()
-
+    train_writer = tensorboardX.SummaryWriter(comment='-train')
+    test_writer = tensorboardX.SummaryWriter(comment='-test')
+    logging.basicConfig(filename=os.path.join(DIR['LOG'], 'test.log'), level=logging.DEBUG,
+                        format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
     args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device {}'.format(args.device))
 
@@ -143,9 +150,34 @@ def main():
     for epoch in range(args.start_epoch, args.epochs):
         adjust_learning_rate(optimizer, epoch, args)
 
-        train(model, train_loader, optimizer, criterion, args)
+        train_loss, train_top1, train_top5 = train(model, train_loader, optimizer, criterion, args)
+        logging.info('[Train] epoch: {}, {}, {}, {}'.format(epoch, train_loss, train_top1, train_top5))
+        train_writer.add_scalar('loss', train_loss.avg, global_step=epoch)
+        train_writer.add_scalar('top1', train_top1.avg, global_step=epoch)
+        train_writer.add_scalar('top5', train_top5.avg, global_step=epoch)
+        test_loss, test_top1, test_top5 = test(model, test_loader, criterion, args)
+        logging.info('[Test] epoch: {}, {}, {}, {}'.format(epoch, test_loss, test_top1, test_top5))
+        test_writer.add_scalar('loss', test_loss.avg, global_step=epoch)
+        test_writer.add_scalar('top1', test_top1.avg, global_step=epoch)
+        test_writer.add_scalar('top5', test_top5.avg, global_step=epoch)
 
-        test(model, test_loader, criterion, args)
+        is_best = test_top1.avg > best_acc1
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'model': args.model,
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict()
+        }, is_best, filename=os.path.join(DIR['CLS_CHECKPOINT'], 'cls_checkpoint_{:03d}.pth.tar'.format(epoch)))
+
+        #check
+        if (epoch + 1) % 10 == 0:
+            sanity_check(model.state_dict(), args.pretrained)
+            logging.info('sanity check passed')
+
+    train_writer.close()
+    test_writer.close()
+    logging.info('training finished')
+
 
 def train(model, data_lodar, optimizer, criterion, args):
     model.eval()
@@ -168,10 +200,29 @@ def train(model, data_lodar, optimizer, criterion, args):
             optimizer.step()
 
             # print('[train] step {}, {}, {}, {}'.format(i, epoch_loss, top1, top5))
-
+    return epoch_loss, top1, top5
 
 def test(model, data_loader, criterion, args):
-    pass
+    epoch_loss = AverageMeter('Loss', ':.6f')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    model.eval()
+
+    with torch.no_grad():
+        for i, (images, labels) in enumerate(data_loader):
+            if args.device == torch.device('cuda'):
+                images, labels = images.cuda(non_blocking=True), labels.cuda(non_blocking=True)
+                output = model(images)
+
+            loss = criterion(output, labels)
+            acc1, acc5 = accuracy(output, labels, topk=(1, 5))
+
+            epoch_loss.maintain(loss.item(), output.shape[0])
+            top1.maintain(acc1, output.shape[0])
+            top5.maintain(acc5, output.shape[0])
+
+    return epoch_loss, top1, top5
+
 
 def adjust_learning_rate(optimizer, epoch, args):
     lr = args.lr
@@ -179,6 +230,26 @@ def adjust_learning_rate(optimizer, epoch, args):
         lr *= 0.1 if epoch >= milestone else 1.
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+def save_checkpoint(state, is_best, filename):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, os.path.join(DIR['CLS_CHECKPOINT'], 'model_best.pth.tar'))
+
+def sanity_check(state_dict, pretrained):
+    print("=> loading '{}' for sanity check".format(pretrained))
+    checkpoint = torch.load(pretrained, map_location='cpu')
+    pretrained_dict = checkpoint['state_dict']
+
+    for k in list(state_dict.keys()):
+        if k == 'fc.weight' or k == 'fc.bias':
+            continue
+        k_pre = 'encoder_q.' + k
+        assert((state_dict[k].cpu() == pretrained_dict[k_pre]).all()), \
+            '{} is changed in linear classifier training'.format(k)
+
+    print("=> sanity check passed")
+
 
 if __name__ == '__main__':
 
